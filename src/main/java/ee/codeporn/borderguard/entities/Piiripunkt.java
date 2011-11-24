f8ea8c66b18dfcb4953ca279255b8d62838bcfbd
msokk <mihkelsokk@gmail.com>
2011-11-24 22:13:29 +0200

    More entities

---
+++ src/main/java/ee/codeporn/borderguard/entities/Piiripunkt.java
@@ -0,0 +1,46 @@
+package ee.codeporn.borderguard.entities;
+
+import org.springframework.roo.addon.entity.RooEntity;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Calendar;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@RooJavaBean
+@RooToString
+@RooEntity
+public class Piiripunkt extends Base {
+
+    @NotNull
+    @Size(max = 20)
+    private String kood;
+
+    @NotNull
+    @Size(max = 100)
+    private String nimetus;
+
+    @NotNull
+    private Float GPSLongitude;
+
+    @NotNull
+    private Float GPSLatitude;
+
+    @NotNull
+    private Float korgusMerepinnast;
+
+    private String kommentaar;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Calendar alates;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Calendar kuni;
+}
