49ae3dc989c33bd734441b6b2c4751b6012ba765
Jaak Lehtsalu <jlehtsalu@hot.ee>
2011-11-24 21:54:04 +0200

    Added more entities

---
+++ src/main/java/ee/codeporn/borderguard/entities/OrgYksus.java
@@ -0,0 +1,36 @@
+package ee.codeporn.borderguard.entities;
+
+import org.springframework.roo.addon.entity.RooEntity;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Calendar;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@RooJavaBean
+@RooToString
+@RooEntity
+public class OrgYksus extends Base {
+
+    @NotNull
+    @Size(max = 20)
+    private String kood;
+
+    @NotNull
+    @Size(max = 100)
+    private String nimetus;
+
+    private String kommentaar;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Calendar alates;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Calendar kuni;
+}
