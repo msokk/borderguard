0e8bc2afee79ddc1b61b7cac07ac64e368bc898b
msokk <mihkelsokk@gmail.com>
2011-11-24 20:52:15 +0200

    Base Entity

---
+++ src/main/java/ee/codeporn/borderguard/entities/Base.java
@@ -0,0 +1,42 @@
+package ee.codeporn.borderguard.entities;
+
+import org.springframework.roo.addon.entity.RooEntity;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.tostring.RooToString;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import org.springframework.format.annotation.DateTimeFormat;
+import java.util.Calendar;
+
+@RooJavaBean
+@RooToString
+@RooEntity
+public abstract class Base {
+    @NotNull
+    @Size(max = 32)
+    private String avaja;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Calendar avatud;
+
+    @NotNull
+    @Size(max = 32)
+    private String muutja;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Calendar muudetud;
+
+    @Size(max = 32)
+    private String sulgeja;
+
+    @NotNull
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(style = "M-")
+    private Calendar suletud;
+}
