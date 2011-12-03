836c67d0618c674d9863a79ddd64184dbef1ed96
Martin Lensment <mlensment@gmail.com>
2011-12-03 16:37:54 +0200

    Removed js, added link

--- src/main/webapp/javascripts/global.js
+++ src/main/webapp/javascripts/global.js
@@ -1,14 +1,7 @@
 $(document).ready(function(){
-	$('#borderViolatorAddNationality').click(function(e){
-		e.preventDefault();
-		window.location = 'kodakondsused?form';
-	});
-	
-	
 	if( typeof $('#editBorderViolatorGender').val() != 'undefined'){
 		var split = $('#editBorderViolatorGender').val().split(',');
 		$('#c_ee_codeporn_borderguard_entities_Piiririkkuja_sugu').val(split.reverse()[0]);
 		$('#editBorderViolatorGender').val(split[0]);
 	}
-
 });
